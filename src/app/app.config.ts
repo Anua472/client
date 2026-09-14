6ca2b81f7927823021e353f29357afd600bd73b6
Anua472 <anabledea@hotmail.es>
2026-09-14 20:58:01 +0200

    Implement the repository pattern

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import 'zone.js';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    // provideZoneChangeDetection(),
+    provideHttpClient()
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./layout/header/header";
+import { Product} from './shared/models/product';
+import { Pagination} from './shared/models/pagination';
+import { Shop } from './core/services/shop';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('client');
+export class App{
+  private shopService = inject(Shop)
+  title = 'Skinet';
+  products= signal<Product[]>([]);
+
+
+  ngOnInit(): void {
+    this.shopService.getProducts().subscribe((data)=>{
+      this.products.set(data)
+    })
+
+
+
+
+  }
+
+
 }
+
+
